031fa6aa2f48b86a7d137b11a85f31b10a042242
danfriedman0 <dan.friedman@yale.edu>
2016-09-09 01:15:22 -0400

    refactored to abtract messaging

--- popup.js
+++ popup.js
@@ -6,19 +6,38 @@
  * extension's content script.
  */
 
-var btn = document.getElementById("restyle");
-var msg = document.getElementById("message");
-btn.addEventListener("click", function() {
+var restyle = document.getElementById("restyle");
+var clearStorage = document.getElementById("clear-storage");
 
+function sendMessage(msg, callback) {
 	// find the active tab and open a connection with the content script
 	chrome.tabs.query({active: true}, function(tabs) {
 		var tabId = tabs[0].id;
 
 		var port = chrome.tabs.connect(tabId, {name: "turnstyle"});
 
-		port.postMessage({instruction: "restyle"});
-		port.onMessage.addListener(function(msg) {
-			message.innerHTML = msg.response;
+		port.postMessage({instruction: msg});
+		port.onMessage.addListener(function(response) {
+			if (callback)
+				callback(response);
 		});		
+	});	
+}
+
+restyle.addEventListener("click", function() {
+	sendMessage("restyle", function(msg) {
+		if (msg.response) {
+			var message = document.getElementById("message");
+			message.innerHTML = msg.response;
+		}
+	});
+});
+
+clearStorage.addEventListener("click", function() {
+	sendMessage("clear storage", function(msg) {
+		if (msg.response) {
+			var message = document.getElementById("message");
+			message.innerHTML = msg.response;
+		}		
 	});
 });
