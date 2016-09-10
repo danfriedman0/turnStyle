d3e957d7ab385da0abc9f4e19d5b5dc52d68f888
danfriedman0 <dan.friedman@yale.edu>
2016-09-10 10:08:51 -0400

    only clear settings for current website

--- turnstyle.js
+++ turnstyle.js
@@ -7,6 +7,49 @@
  * Dan Friedman
  */
 
+var styleString = "@import 'https://fonts.googleapis.com/css?family=Roboto:300,400,500'\n" +
+	"body { margin: 0 auto; max-width: 50em; font-family: 'Roboto', Helvetica', 'Arial', sans-serif; line-height: 1.5; padding: 4em 1em; color: #444 }\n" +
+	"h2 { margin-top: 1em; padding-top: 1em }\n" +
+	"h1,h2,strong { color: #333; }\n" +
+	"code,pre { background: #f5f7f9; border-bottom: 1px solid #d8dee9; }\n" +
+	"code { padding: 2px 4px; vertical-align: text-bottom; }\n" +
+	"pre { padding: 1em; border-left: 2px solid #69c; }\n";
+
+var styleObj = {
+	"@import": "https://fonts.googleapis.com/css?family=Roboto:300,400,500",
+	"body": {
+		"margin": "0 auto",
+		"max-width": "50em",
+		"font-family": "'Roboto', 'Helvetica', 'Arial', sans-serif'",
+		"line-height": "1.5",
+		"padding": "4em 1em",
+		"color": "#444"
+	},
+	"h2": {
+		"margin-top": "1em",
+		"padding-top": "1em",
+		"color": "#333"
+	},
+	"h1": {
+		"color": "#333"
+	},
+	"strong": {
+		"color": "#333"
+	},
+	"code": {
+		"background": "#f5f7f9",
+		"border-bottom": "1px solid #d8dee9",
+		"padding": "2px 4px",
+		"vertical-align": "text-bottom"
+	},
+	"pre": {
+		"background": "#f5f7f9",
+		"border-bottom": "1px solid #d8dee9",
+		"padding": "1em",
+		"border-left": "2px solid #69c"
+	}
+}
+
 /**
  * restyle: overrides the page's style by inserting the specified stylesheet
  * @param {String} stylesheet: the name of a stylesheet
@@ -46,21 +89,34 @@ function loadSettings() {
 			var stylesheet = obj ? obj.turnStyle[pageUrl] : null;
 			if (stylesheet) {
 				restyle(stylesheet);
-				chrome.runtime.sendMessage({
-					message: "loaded settings",
-					url: pageUrl,
-					stylesheet: stylesheet
-				});
 			}
 		}
 	});
 }
 
 /**
- * clearStorage: clear any saved settings in chrome.storage
+ * clearStorage: clear any of the settings saved for this website and reload the page
+ * @param {Boolean} clearAll: the function will clear all turnStyle settings if clearAll == True
+ * @param {Boolean} reload: reload the page unless reload == False
  */
-function clearStorage() {
-	chrome.storage.sync.remove("turnStyle");	
+function clearStorage(clearAll, reload) {
+	if (clearAll) {
+		chrome.storage.sync.remove("turnStyle");	
+	}
+	else {
+		var pageUrl = location.origin;
+		chrome.storage.sync.get("turnStyle", function(obj) {
+			var turnStyle = obj ? obj.turnStyle : null;
+			if (turnStyle) {
+				var pageUrl = location.origin;
+				delete turnStyle[pageUrl];
+				chrome.storage.sync.set({"turnStyle": turnStyle});
+			}
+		});
+	}
+	if (reload || reload == null)
+		location.reload();
+
 }
 
 // Listen for messages from popup.js
