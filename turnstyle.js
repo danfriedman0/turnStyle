97d50c419d6aa3c04d04d27bcff13e7c6ea12795
danfriedman0 <dan.friedman@yale.edu>
2016-09-10 14:24:24 -0400

    store styles in a separate object for reusability

--- turnstyle.js
+++ turnstyle.js
@@ -7,48 +7,82 @@
  * Dan Friedman
  */
 
-var styleString = "body { margin: 0 auto; max-width: 50em; font-family: 'Roboto', 'Helvetica', 'Arial', sans-serif; line-height: 1.5; padding: 4em 1em; color: #444 }\n" +
+var defaultStyle = "body { margin: 0 auto; max-width: 50em; font-family: 'Roboto', 'Helvetica', 'Arial', sans-serif; line-height: 1.5; padding: 4em 1em; color: #444 }\n" +
 	"h2 { margin-top: 1em; padding-top: 1em }\n" +
 	"h1,h2,strong { color: #333; }\n" +
 	"code,pre { background: #f5f7f9; border-bottom: 1px solid #d8dee9; }\n" +
 	"code { padding: 2px 4px; vertical-align: text-bottom; }\n" +
 	"pre { padding: 1em; border-left: 2px solid #69c; }\n";
 
+
 /**
  * restyle: overrides the page's style by inserting the css rules
- * @param {String} style: CSS rules
+ * @param {String} styleRules
  */
-function restyle(style) {
-	var node = document.createElement("style");
-	node.setAttribute("class", "turnstyle");
-	node.innerHTML = style;
-	document.head.appendChild(node);
+function restyle(styleRules) {
+	if (styleRules) {
+		var node = document.createElement("style");
+		node.setAttribute("class", "turnstyle");
+		node.innerHTML = styleRules;
+		document.head.appendChild(node);		
+	}
 }
 
 /**
- * saveStyle: save the settings for this website with the Chrome storage API
- * @param {String} style: CSS rules
+ * getStyle: get a string of style rules from the styles object in chrome storage
+ * 		and call the callback function on the result
+ * @param {String} styleName
+ * @param {Function} callback
  */
-function saveStyle(style) {
+function getStyle(styleName, callback) {
+	chrome.storage.sync.get("styles", function(result) {
+		if (result.styles) {
+			var styleRules = result.styles[styleName] ? result.styles[styleName] : "";
+			callback(styleRules);
+		}
+	});
+}
+
+
+/**
+ * saveStyle: save the style in the styles object in storage
+ * @param {String} styleName
+ * @param {String} styleRules
+ */
+function saveStyle(styleName, styleRules, overWrite) {
+	chrome.storage.sync.get("styles", function(result) {
+		var styles = result.styles ? result.styles : {};
+		if (overWrite || !styles[styleName]) {
+			styles[styleName] = styleRules;
+			chrome.storage.sync.set({"styles": styles});
+		}
+	});
+}
+
+/**
+ * saveSettings: save the settings for this website in chrome storage
+ * @param {String} styleName
+ */
+function saveSettings(styleName) {
 	var pageUrl = location.origin;
-	var settings = {};
-	settings[pageUrl] = style;
-	chrome.storage.sync.set(settings);
+	if (pageUrl !== "styles") {			// make sure we don't overwrite the styles field
+		var settings = {};
+		settings[pageUrl] = styleName;
+		chrome.storage.sync.set(settings);		
+	}
 }
 
 /**
  * loadSettings: look for saved settings in chrome.storage; if there are any, restyle the
- * page and send a message to the popup script to change the popup text
+ * 		page and send a message to the popup script to change the popup text
  */
 function loadSettings() {
 	var pageUrl = location.origin;
-	chrome.storage.sync.get(pageUrl, function(obj) {
-		if (obj && obj[pageUrl]) {
-			restyle(obj[pageUrl])
+	chrome.storage.sync.get(pageUrl, function(result) {
+		if (result[pageUrl]) {
+			getStyle(result[pageUrl], restyle);
 		}
 	});
-
-	getStorageInfo();
 }
 
 /**
@@ -76,23 +110,22 @@ function clearStorage(clearAll) {
 }
 
 function getStorageInfo() {
-	chrome.storage.sync.getBytesInUse("turnStyle", function(bytes) {
+	chrome.storage.sync.getBytesInUse(null, function(bytes) {
 		console.log(bytes + " bytes in use");
 	});
 	chrome.storage.sync.get(null, function(storage) {
-		for (var key in storage) {
-			if (storage.hasOwnProperty(key))
-				console.log(key + " -> " + storage[key]);
-		}
+		console.log(storage);
 	});
 }
 
-// Listen for messages from popup.js
+// listen for messages from popup.js
 chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
 	if (request.instruction === "restyle") {
-		var style = request.style ? request.style : styleString;
-		restyle(style);
-		saveStyle(style);
+		var styleName = request.styleName ? request.styleName : "default";
+		var styleRules = request.styleRules ? request.styleRules : defaultStyle;
+		saveStyle(styleName, styleRules);
+		restyle(styleRules);
+		saveSettings(styleName);
 		sendResponse({message: "restyled"})
 	}
 	else if (request.instruction === "clear settings") {
