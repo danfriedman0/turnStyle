8716a7175040e75612196cbf6a732bbac34deaf8
danfriedman0 <dan.friedman@yale.edu>
2016-09-11 16:25:06 -0400

    store client script in an object to minimize lookup calls

--- turnstyle.js
+++ turnstyle.js
@@ -7,18 +7,49 @@
  * Dan Friedman
  */
 
-var defaultStyle = "body { margin: 0 auto; max-width: 50em; font-family: 'Roboto', 'Helvetica', 'Arial', sans-serif; line-height: 1.5; padding: 4em 1em; color: #444 }\n" +
-	"h2 { margin-top: 1em; padding-top: 1em }\n" +
-	"h1,h2,strong { color: #333; }\n" +
-	"code,pre { background: #f5f7f9; border-bottom: 1px solid #d8dee9; }\n" +
-	"code { padding: 2px 4px; vertical-align: text-bottom; }\n" +
-	"pre { padding: 1em; border-left: 2px solid #69c; }\n";
+var TurnStyle = function() {
+	this.pageUrl = location.origin;
+	this.pageSettings = [];
+	this.styles = {};
 
+	this.initialize();
+}
+
+/**
+ * loadStyles: load all saved styles into the styles object
+ */
+TurnStyle.prototype.loadStyles = function() {
+	var me = this;
+	chrome.storage.sync.get("styles", function(result) {
+		if (result.styles) {
+			me.styles = result.styles;
+		}
+	});
+}
+
+/**
+ * loadSettings: look for saved styles in chrome.storage and restyle the page if there are any
+ */
+TurnStyle.prototype.loadPageSettings = function() {
+	var pageUrl = this.pageUrl;
+	var me = this;
+
+	chrome.storage.sync.get(pageUrl, function(result) {
+		if (result[pageUrl]) {
+			me.pageSettings = result[pageUrl];
+			result[pageUrl].forEach(function(styleName) {
+				var styleRules = me.styles[styleName];
+				if (styleRules)
+					me.insertStyle(styleRules);
+			});
+		}
+	});
+}
 
 /**
  * insertStyle: override the page's style by inserting the css rules with optional class name and id
  */
-function insertStyle(styleRules, className, id) {
+TurnStyle.prototype.insertStyle = function(styleRules, className, id) {
 	if (styleRules) {
 		var node = document.createElement("style");
 		node.setAttribute("class", "turnstyle");
@@ -35,7 +66,7 @@ function insertStyle(styleRules, className, id) {
  * removeStyle: remove all styles with the specified id or class name
  *	if no arguments are passed, remove all style elements with the "turnstyle" class
  */
-function removeStyle(styleId, className) {
+TurnStyle.prototype.removeStyle = function(styleId, className) {
 	if (styleId) {
 		var styleNode = document.getElementById(styleId);
 		if (styleNode)
@@ -50,69 +81,36 @@ function removeStyle(styleId, className) {
 	}
 }
 
-/**
- * getStyle: get a string of style rules from the styles object in chrome storage
- * 	and call the callback function on the result
- */
-function getStyle(styleName, callback) {
-	chrome.storage.sync.get("styles", function(result) {
-		if (result.styles) {
-			var styleRules = result.styles[styleName] ? result.styles[styleName] : "";
-			callback(styleRules);
-		}
-	});
-}
-
 
 /**
  * saveStyle: save the style in the styles object in storage
  *	overwrite the existing style if overwrite is set to true
  */
-function saveStyle(styleName, styleRules, overWrite) {
-	chrome.storage.sync.get("styles", function(result) {
-		var styles = result.styles ? result.styles : {};
-		if (overWrite || !styles[styleName]) {
-			styles[styleName] = styleRules;
-			chrome.storage.sync.set({"styles": styles});
-		}
-	});
+TurnStyle.prototype.saveStyle = function(styleName, styleRules, overWrite) {
+	var styles = this.styles;
+	if (overWrite || !styles[styleName]) {
+		styles[styleName] = styleRules;
+		chrome.storage.sync.set({"styles": styles});
+	}
 }
 
 /**
  * saveSettings: save the settings for this website in chrome storage
  */
-function saveSettings(styleName) {
-	var pageUrl = location.origin;
+TurnStyle.prototype.saveSettings = function(styleName) {
+	var pageUrl = this.pageUrl;
 	if (pageUrl !== "styles") {			// make sure we don't overwrite the styles field
-		chrome.storage.sync.get(pageUrl, function(result) {
-			var pageStyles = result[pageUrl] ? result[pageUrl] : [];
-			var pageEntry = {};
-			pageStyles.push(styleName);
-			pageEntry[pageUrl] = pageStyles;
-			chrome.storage.sync.set(pageEntry);
-			getStorageInfo();
-		});
+		var pageEntry = {};
+		this.pageSettings.push(styleName);
+		pageEntry[pageUrl] = this.pageSettings;
+		chrome.storage.sync.set(pageEntry);
 	}
-}
-
-/**
- * loadSettings: look for saved styles in chrome.storage and restyle the page if there are any
- */
-function loadSettings() {
-	var pageUrl = location.origin;
-	chrome.storage.sync.get(pageUrl, function(result) {
-		if (result[pageUrl]) {
-			result[pageUrl].forEach(function(styleRules) {
-				getStyle(styleRules, insertStyle);
-			});
-		}
-	});
 }
 
 /**
  * clearStorage: clear any of the settings saved for this website
  */
-function clearStorage(clearAll) {
+TurnStyle.prototype.clearStorage = function(clearAll) {
 	if (clearAll) {
 		chrome.storage.sync.get(null, function(obj) {
 			for (var key in obj) {
@@ -122,14 +120,13 @@ function clearStorage(clearAll) {
 		});
 	}
 	else {
-		var pageUrl = location.origin;
-		chrome.storage.sync.remove(pageUrl);
+		chrome.storage.sync.remove(this.pageUrl);
 	}
 
-	removeStyle();
+	this.removeStyle();
 }
 
-function getStorageInfo() {
+TurnStyle.prototype.getStorageInfo = function() {
 	chrome.storage.sync.getBytesInUse(null, function(bytes) {
 		console.log(bytes + " bytes in use");
 	});
@@ -139,45 +136,60 @@ function getStorageInfo() {
 }
 
 // listen for messages from popup.js
-chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
-	if (request.instruction === "insertStyle") {
-		var styleRules = request.styleRules ? request.styleRules : "";
-		var styleId = request.styleId ? request.styleId : "";
-		var className = request.className ? request.className : "";
+TurnStyle.prototype.addListener = function() {
+	var me = this;
 
-		if (styleId === "ts-preview")
-			removeStyle("ts-preview");
+	chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
+		if (request.instruction === "insertStyle") {
+			var styleRules = request.styleRules ? request.styleRules : "";
+			var styleId = request.styleId ? request.styleId : "";
+			var className = request.className ? request.className : "";
 
-		insertStyle(styleRules, className, styleId);
-		sendResponse({message: "inserted style"});
-	}
-	else if (request.instruction === "removeStyle") {
-		var styleId = request.styleId ? request.styleId : null;
-		var className = request.className ? request.className : null;
-		removeStyle(styleId, className);
-		sendResponse({message: "removed style"});
-	}
-	else if (request.instruction === "restyle") {
-		var styleRules = request.styleRules ? request.styleRules : defaultStyle;
-		var styleName = request.styleName ? request.styleName : "default";
-		
-		insertStyle(styleRules);
-		saveStyle(styleName, styleRules);
-		saveSettings(styleName);
-		sendResponse({message: "restyled"})
-	}
-	else if (request.instruction === "clear settings") {
-		clearStorage();
-		sendResponse({message: "settings cleared"});
-	}
-	else if (request.instruction === "clear all") {
-		clearStorage(true);
-		sendResponse({message: "storage cleared"})
-	}
-});
+			if (styleId === "ts-preview")
+				me.removeStyle("ts-preview");
 
-// on load
-loadSettings();
+			me.insertStyle(styleRules, className, styleId);
+			sendResponse({message: "inserted style"});
+		}
+
+		else if (request.instruction === "removeStyle") {
+			var styleId = request.styleId ? request.styleId : null;
+			var className = request.className ? request.className : null;
+			me.removeStyle(styleId, className);
+			sendResponse({message: "removed style"});
+		}
+
+		else if (request.instruction === "restyle") {
+			if (request.styleRules && request.styleName) {
+				me.insertStyle(request.styleRules);
+				me.saveStyle(request.styleName, request.styleRules);
+				me.saveSettings(request.styleName);
+				sendResponse({message: "restyled"});				
+			}
+		}
+
+		else if (request.instruction === "clear settings") {
+			me.clearStorage();
+			sendResponse({message: "settings cleared"});
+		}
+
+		else if (request.instruction === "clear all") {
+			me.clearStorage(true);
+			sendResponse({message: "storage cleared"})
+		}
+	});	
+}
+
+
+TurnStyle.prototype.initialize = function() {
+	this.loadStyles();
+	this.loadPageSettings();
+	this.addListener();
+	console.log(this);
+}
+
+var turnStyle = new TurnStyle();
+
 
 
 
